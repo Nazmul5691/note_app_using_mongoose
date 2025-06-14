bd40df823b89b3ddfb8ce6b7306b22ac31701462
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-15 01:37:28 +0600

    required field in schema

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,27 @@ import mongoose, { Schema } from 'mongoose';
 const app: Application = express();
 
 const notesSchema = new Schema({
-    title: String,
-    content: String
+    // title: String,
+    // content: String
+    title: { type: String, required: true, trim: true},
+    content: { type: String, default: ''},
+    category: {
+        type: String,
+        enum: ["personal", "work", "study", "other"],
+        default: "personal"
+    },
+    pinned: {
+        type: Boolean,
+        default: false
+    }
 })
 
 const Note = mongoose.model("Note", notesSchema);
 
-app.post('/create-note', async (req: Request, res: Response) =>{
+app.post('/create-note', async (req: Request, res: Response) => {
     const myNote = new Note({
-         title: 'Learning Mongoose',
-         content: 'I am learning Mongoose',
+        title: 'Learning Mongoose',
+        
     })
 
     await myNote.save()
@@ -22,11 +33,11 @@ app.post('/create-note', async (req: Request, res: Response) =>{
     res.status(201).json({
         success: true,
         message: "Note created successfully",
-        note : myNote
+        note: myNote
     })
 })
 
-app.get('/', (req: Request, res: Response) =>{
+app.get('/', (req: Request, res: Response) => {
     res.send("welcome to note app")
 })
 
