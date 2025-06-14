4eef0cc0520f65e7b23c622680bd467d774a40f1
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-14 23:36:48 +0600

    create schema and model

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,33 @@
 import express, { Application, Request, Response } from 'express'
+import mongoose, { Schema } from 'mongoose';
+
 
 const app: Application = express();
 
+const notesSchema = new Schema({
+    title: String,
+    content: String
+})
+
+const Note = mongoose.model("Note", notesSchema);
+
+app.post('/create-note', async (req: Request, res: Response) =>{
+    const myNote = new Note({
+         title: 'Learning Mongoose',
+         content: 'I am learning Mongoose',
+    })
+
+    await myNote.save()
+
+    res.status(201).json({
+        success: true,
+        message: "Note created successfully",
+        note : myNote
+    })
+})
+
 app.get('/', (req: Request, res: Response) =>{
-    res.send("welcome to noteee app")
+    res.send("welcome to note app")
 })
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -9,10 +9,10 @@ const PORT = 5000;
 
 async function main(){
     try {
-        await mongoose.connect('mongodb+srv://noteApp:noteApp@cluster0.laemifb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+        await mongoose.connect('mongodb+srv://noteApp:noteApp@cluster0.laemifb.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0');
 
         console.log('connected to mongodb using mongoose');
-        
+
         server = app.listen(PORT, () =>{
             console.log(`App is listen on port ${PORT}`);
         })
