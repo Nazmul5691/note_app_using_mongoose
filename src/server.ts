7565076077f79d0866628481431a852ce91378ab
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-14 22:34:11 +0600

    connected to mongodb

--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,13 @@ let server: Server;
 
 const PORT = 5000;
 
+
 async function main(){
     try {
         await mongoose.connect('mongodb+srv://noteApp:noteApp@cluster0.laemifb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
         console.log('connected to mongodb using mongoose');
+        
         server = app.listen(PORT, () =>{
             console.log(`App is listen on port ${PORT}`);
         })
