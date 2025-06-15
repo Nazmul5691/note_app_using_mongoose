9e6aac5faec3abbda0fa1eaf7fef3fd8289a332c
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-15 21:48:50 +0600

    create user and get user

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -14,3 +14,14 @@ userRoutes.post('/create-user', async (req: Request, res: Response) =>{
         user
     })
 })
+
+
+userRoutes.get('/', async (req: Request, res: Response) =>{
+    const users = await User.find();
+
+    res.status(201).json({
+        success: true,
+        message: 'users get successfully',
+        users
+    })
+})
