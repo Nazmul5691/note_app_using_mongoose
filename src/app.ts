ca98d7bfdbbf8cc81ab4eea98c4b97916aad8c17
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-15 19:45:28 +0600

    version key and timestamp

--- src/app.ts
+++ src/app.ts
@@ -7,25 +7,32 @@ const app: Application = express();
 
 app.use(express.json());
 
-const notesSchema = new Schema({
-    // title: String,
-    // content: String
-    title: { type: String, required: true, trim: true },
-    content: { type: String, default: '' },
-    category: {
-        type: String,
-        enum: ["personal", "work", "study", "other"],
-        default: "personal"
+const notesSchema = new Schema(
+    {
+        // title: String,
+        // content: String
+        title: { type: String, required: true, trim: true },
+        content: { type: String, default: '' },
+        category: {
+            type: String,
+            enum: ["personal", "work", "study", "other"],
+            default: "personal"
+        },
+        pinned: {
+            type: Boolean,
+            default: false
+        },
+        tags: {
+            label: { type: String, required: true },
+            color: { type: String, default: "gray" }
+        }
     },
-    pinned: {
-        type: Boolean,
-        default: false
-    },
-    tags: {
-        label: { type: String, required: true },
-        color: { type: String, default: "gray" }
+    {
+        versionKey: false,
+        timestamps: true
     }
-})
+    
+)
 
 const Note = mongoose.model("Note", notesSchema);
 
