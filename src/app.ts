c328ea08cb6c4ff1503409b21688b9d467cfa4a1
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-15 21:42:10 +0600

    create user route

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, { Application, Request, Response } from 'express'
 import { notesRoutes } from './app/controllers/notes.controller';
+import { userRoutes } from './app/controllers/user.controller';
 
 const app: Application = express();
 
 app.use(express.json());
 
-app.use("/notes", notesRoutes)
+app.use("/notes", notesRoutes);
+app.use("/users", userRoutes)
 
 app.get('/', (req: Request, res: Response) => {
     res.send("welcome to note app")
