c0d745067ff0677c8a597aeb7b1864c9c0caeeaf
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-16 22:20:39 +0600

    referencing

--- src/app/models/notes.model.ts
+++ src/app/models/notes.model.ts
@@ -26,6 +26,11 @@ const notesSchema = new Schema<INotes>(
         tags: {
             label: { type: String, required: true },
             color: { type: String, default: "gray" }
+        },
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true
         }
     },
     {
