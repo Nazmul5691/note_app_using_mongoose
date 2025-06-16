a8ca421dfb9652d132d513ace05921528b3f5497
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-06-16 22:29:47 +0600

    population-in-mongoose

--- src/app/controllers/notes.controller.ts
+++ src/app/controllers/notes.controller.ts
@@ -41,7 +41,7 @@ notesRoutes.post('/create-note', async (req: Request, res: Response) => {
 //get all notes
 notesRoutes.get('/', async (req: Request, res: Response) => {
 
-    const notes = await Note.find()
+    const notes = await Note.find().populate("user")
 
     res.status(201).json({
         success: true,
